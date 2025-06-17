37f832ccc56adfb35d563242dfe7f27e6e64e1e4
yaoshiluo <171806458@qq.com>
2025-06-17 23:21:01 +0200

    readme变动

--- catkin_ws/build/minimind_chat_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/AdvRoboCup/catkin_ws/src/minimind_chat_ros
-# Build directory: /home/ubuntu/AdvRoboCup/catkin_ws/build/minimind_chat_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
